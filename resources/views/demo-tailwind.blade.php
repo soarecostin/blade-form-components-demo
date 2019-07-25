1cfe6b2f662114b4ed4dab33197cc6a5f026cec6
Costin Soare <soarecostin@gmail.com>
2019-07-25 17:19:28 +0200

    Add tailwind addons support

--- resources/views/demo-tailwind.blade.php
+++ resources/views/demo-tailwind.blade.php
@@ -12,12 +12,14 @@
     <body class="bg-gray-100">
         <div id="app">
             <div class="container mx-auto py-10 flex justify-center items-center">
-
                 <div class="w-full lg:w-3/5 rounded bg-white">
                     <div class="p-6">
-
-                        @include ("form")
-
+                        @include ('form', [
+                            'priceAddons' => [
+                                'prepend' => '<select class="block appearance-none border border-gray-200 text-gray-700 py-3 px-4 mb-2 rounded border-r-0 rounded-r-none leading-tight"><option>$</option><option>£</option><option>€</option></select>',
+                                'append' => '<span class="block border border-gray-200 text-gray-700 py-3 px-4 mb-2 rounded border-l-0 rounded-l-none leading-tight">.00</span>'
+                            ]
+                        ])
                     </div>
                 </div>
             </div>
